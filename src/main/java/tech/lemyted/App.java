60eb1b793f19a11ed5f9cf7f67241d759cc788d2
ted <lemyted21@gmail.com>
2024-11-27 17:15:58 -0500

    Fixed jar execution

--- src/main/java/tech/lemyted/App.java
+++ src/main/java/tech/lemyted/App.java
@@ -3,13 +3,16 @@ package tech.lemyted;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import tech.lemyted.converter.DecimalConverterS32;
 import tech.lemyted.converter.DecimalConverterU32;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/converter")
 public class App 
 {
     public static void main( String[] args )
@@ -18,14 +21,26 @@ public class App
     }
     
     @GetMapping("/bin-to-dec-u32")
-    public long convertBinToDec(@RequestParam String binStr) 
+    public long convertBinToDecU(@RequestParam String binStr) 
     {
     	return DecimalConverterU32.convert(binStr);
     }
     
     @GetMapping("/dec-to-bin-u32")
-    public String convertDecToBin(@RequestParam long decNum) 
+    public String convertDecToBinU(@RequestParam long decNum) 
     {
     	return DecimalConverterU32.toBinary(decNum);
+    }
+    
+    @GetMapping("/bin-to-dec-s32")
+    public long convertBinToDecS(@RequestParam String binStr) 
+    {
+    	return DecimalConverterS32.convert(binStr);
+    }
+    
+    @GetMapping("/dec-to-bin-s32")
+    public String convertDecToBinS(@RequestParam long decNum) 
+    {
+    	return DecimalConverterS32.toBinary(decNum);
     }
 }
